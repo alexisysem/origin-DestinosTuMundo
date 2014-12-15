32720f35ce26c77dcbe2ce503c8abeea0ee26a15
alexisysem <alexisystem@gmail.com>
2014-12-15 02:28:07 -0500

    Agregué la primera version de multiidioma
    primer avance

---
+++ Inicio.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>Pagina principal</title>
+
+<script type="text/javascript" src="js/jquery-1.7.min.js"></script>
+<script type="text/javascript" src="js/superfish.js"></script>
+
+
+<link rel="stylesheet" type="text/css" href="css/font-awesome.css" media="all"/>
+<link rel="stylesheet" type="text/css" href="css/jquery.bxslider.css" media="all"/>
+<link rel="stylesheet" type="text/css" href="css/photoswipe.css" media="all"/>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css" media="all"/>
+<link rel="stylesheet" type="text/css" href="css/extra_style.css" media="all"/>
+<link rel="stylesheet" type="text/css" href="css/style.css" media="all"/>
+<link rel="stylesheet" type="text/css" href="css/responsive.css" media="all"/>
+<link rel="stylesheet" type="text/css" href="css/superfish.css" media="all"/>
+<link rel="stylesheet" type="text/css" href="css/camera.css" media="all"/>
+<link rel="stylesheet" type="text/css" href="css/widgets.css" media="all"/>
+<link rel="stylesheet" type="text/css" href="css/cloud-zoom.css" media="all"/>
+<link rel="stylesheet" type="text/css" href="css/catalogsale.css" media="all"/>
+<link rel="stylesheet" type="text/css" href="css/print.css" media="print"/>
+
+
+</head>
+<body class="ps-static  cms-index-index cms-home">
+<div class="wrapper ps-static en-lang-class">
+	<div class="page">
+	<div class="top-container">
+		<div class="container">
+		<div class="row">
+		<div class="col-xs-12">
+		<div class="top_block">
+		<p class="welcome-msg">Welcome to our travel agency! </p>
+		<div class="header-buttons">
+		<div class="header-button currency-list">
+		<a title="Spanish" href="test.php?Lan=EN"> 
+			<img src="images/flag_pe.png" />
+		</a>		
+		</div>
+		<div class="header-button lang-list">
+		<a title="English" href="test.php?Lan=ES">
+			<img src="images/flag_us.png" />
+		</a>		
+		</div>
+		</div>
+		</div>
+		</div>
+		</div>
+		<div class="clear"></div>
+		</div>
+	</div>
+	<div class="header-container-tile">
+	
+		<div class="header-container">
+		<div class="container">
+		<div class="row">
+		<div class="col-xs-12">
+		<div class="header">
+		<h1 class="logo">
+			<strong>Destinos Tu Mundo</strong>
+			<a href="index.php" title="Destinos Tu Mundo">
+				<img src="images/logo.png" alt="Destinos Tu Mundo">
+			</a>
+		</h1>
+		<div class="right_head">
+		<div class="quick-access hidden">
+		<ul class="links">
+		<li class="first link-1"><a href="http://livedemo00.template-help.com/magento_47319/customer/account/" title="My Account">My Account</a></li>
+		<li class="link-2"><a href="http://livedemo00.template-help.com/magento_47319/wishlist/" title="My Wishlist">My Wishlist</a></li>
+		<li class="link-3 top-car"><a href="http://livedemo00.template-help.com/magento_47319/checkout/cart/" title="My Cart" class="top-link-cart">My Cart</a></li>
+		<li class="link-4"><a href="http://livedemo00.template-help.com/magento_47319/checkout/" title="Checkout" class="top-link-checkout">Checkout</a></li>
+		<li class="last link-5"><a href="http://livedemo00.template-help.com/magento_47319/customer/account/login/" title="Log In">Log In</a></li>
+		</ul>
+		</div>
+		<div class="block-cart-header hidden">
+		<h3>My Cart:</h3>
+		<div class="block-content">
+		<div class="empty">
+		<div>0 item(s) - <span class="price">$0.00</span></div>
+		<div class="cart-content">
+		You have no items in your shopping cart. </div>
+		</div>
+		<p class="mini-cart"><strong>0</strong></p>
+		</div>
+		</div>
+		<form id="search_mini_form" action="http://livedemo00.template-help.com/magento_47319/catalogsearch/result/" method="get">
+		<div class="form-search">
+		<label for="search">Search:</label>
+		<input id="search" type="text" name="q" value="" class="input-text" autocomplete="off">
+		<button type="submit" title="Search" class="button"><span><span>Search</span></span></button>
+		<div id="search_autocomplete" class="search-autocomplete" style="display: none;"></div>
+		 
+		
+		</div>
+		</form>
+		<style>.ajaxsearch{border:solid #CCCCCC 1px}.ajaxsearch .suggest{background:#0A263D;color:#B4B4B4}.ajaxsearch .suggest .amount{color:#FF0000}.ajaxsearch .preview{background:#ffffff}.ajaxsearch .preview a{color:#1B43AC}.ajaxsearch .preview .description{color:#0A263D}.ajaxsearch .preview img{float:left;border:solid 1px #CCC}.header .form-search .ajaxsearch li.selected{background-color:#FBFBFB}</style> 
+		</div>
+		<div class="clear"></div>
+		<div class="nav-container">
+		<ul id="nav" class="sf-menu sf-js-enabled">
+		<li class="level0 nav-1 first level-top">
+			<a href="aruba.html" class="level-top"><span>Inicio</span></a>
+		</li>
+		<li class="level0 nav-1 first level-top">
+			<a href="aruba.html" class="level-top"><span>Nacionales</span></a>
+		</li>
+		<li class="level0 nav-1 first level-top">
+			<a href="aruba.html" class="level-top"><span>Internacionales</span></a>
+		</li>
+		<li class="level0 nav-2 level-top">
+			<a href="cancun.html" class="level-top"><span>Tour</span></a>
+		 </li>
+		 <li class="level0 nav-2 level-top">
+			<a href="cancun.html" class="level-top"><span>Pasajes</span></a>
+		 </li>
+		 <li class="level0 nav-2 level-top">
+			<a href="cancun.html" class="level-top"><span>Contáctenos</span></a>
+		 </li>
+		 
+		</ul>
+		<div class="sf-menu-block">
+		<div id="menu-icon">Categories</div>
+		<ul class="sf-menu-phone">
+		<li class="level0 nav-1 first level-top"><a href="http://livedemo00.template-help.com/magento_47319/aruba.html" class="level-top"><span>Aruba</span></a></li><li class="level0 nav-2 level-top"><a href="http://livedemo00.template-help.com/magento_47319/cancun.html" class="level-top"><span>Cancun</span></a></li><li class="level0 nav-3 level-top parent"><a href="http://livedemo00.template-help.com/magento_47319/florida.html" class="level-top"><span>Florida</span></a><ul class="level0"><li class="level1 nav-3-1 first"><a href="http://livedemo00.template-help.com/magento_47319/florida/eco-tours.html"><span>Eco tours</span></a></li><li class="level1 nav-3-2"><a href="http://livedemo00.template-help.com/magento_47319/florida/family-tours.html"><span>Family tours</span></a></li><li class="level1 nav-3-3 last"><a href="http://livedemo00.template-help.com/magento_47319/florida/hot-tours.html"><span>Local tours</span></a></li></ul><strong></strong></li><li class="level0 nav-4 level-top"><a href="http://livedemo00.template-help.com/magento_47319/hawaii.html" class="level-top"><span>Hawaii</span></a></li><li class="level0 nav-5 level-top"><a href="http://livedemo00.template-help.com/magento_47319/myrtle-beach.html" class="level-top"><span>Myrtle beach</span></a></li><li class="level0 nav-6 last level-top"><a href="http://livedemo00.template-help.com/magento_47319/other.html" class="level-top"><span>Other</span></a></li> </ul>
+		</div>
+		</div>
+		
+			<div class="container">
+			<div class="row">
+			<div class="col-xs-12">
+			<div class="top_block">
+			<p class="welcome-msg">Welcome to our online store! </p>
+			<div class="header-buttons">
+			<div class="header-button currency-list">
+			<a title="Currency" href="#">$</a>
+			<ul>
+			<li>
+			<a href="http://livedemo00.template-help.com/magento_47319/directory/currency/switch/currency/EUR/uenc/aHR0cDovL2xpdmVkZW1vMDAudGVtcGxhdGUtaGVscC5jb20vbWFnZW50b180NzMxOS8,/" title="EUR">Euro - EUR</a>
+			</li>
+			<li>
+			<a class="selected" href="http://livedemo00.template-help.com/magento_47319/directory/currency/switch/currency/USD/uenc/aHR0cDovL2xpdmVkZW1vMDAudGVtcGxhdGUtaGVscC5jb20vbWFnZW50b180NzMxOS8,/" title="USD">US Dollar - USD</a>
+			</li>
+			</ul>
+			</div>
+			<div class="header-button lang-list">
+			<a title="Language" href="#">en</a>
+			<ul>
+			<li>
+			<a class="selected" href="http://livedemo00.template-help.com/magento_47319/?___store=english&amp;___from_store=english" title="en_US">English</a>
+			</li>
+			<li>
+			<a href="http://livedemo00.template-help.com/magento_47319/?___store=german&amp;___from_store=english" title="de_DE">German</a>
+			</li>
+			<li>
+			<a href="http://livedemo00.template-help.com/magento_47319/?___store=spanish&amp;___from_store=english" title="es_ES">Spanish</a>
+			</li>
+			<li>
+			<a href="http://livedemo00.template-help.com/magento_47319/?___store=russian&amp;___from_store=english" title="ru_RU">Russian</a>
+			</li>
+			</ul>
+			</div>
+			</div>
+			</div>
+			</div>
+			</div>
+			<div class="clear"></div>
+			</div>
+		</div>
+		 
+		
+		
+		
+		
+		</div>
+		</div>
+		</div>
+		 
+		</div> </div>
+	
+	
+	</div>
+	</div>
+	<div class="clear"></div>
+</div>		
+
+</div>
+</body>
+</html>
--- test.php
+++
@@ -1,8 +0,0 @@
-<html>
- <head>
-  <title>Prueba de PHP</title>
- </head>
- <body>
- <?php echo '<p>Hola Mundo 285445</p>'; ?>
- </body>
-</html>
